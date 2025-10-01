94aaec725d3e86b2ba50343cdf6f0cae9b19435c
raffay <sunnetsystems19.com>
2025-10-01 16:48:30 +0530

    init boilerplate server

---
+++ models/models.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export async function initDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to DB");
+  } catch (e) {
+    console.log("error connecting to db", e.message);
+  }
+}
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import "dotenv/config";
+import productRoutes from "./routes/productsRoute";
+import servicesRoutes from "./routes/servicesRoute";
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+app.use("/products", productRoutes);
+app.use("/services", servicesRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
